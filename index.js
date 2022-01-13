5ed7db860bb6e6319c8d41427d284e2451eefabe
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2022-01-14 01:00:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const fs = require('fs/promises');
 const path = require('path');
 
 // --------work with all files ------------
-const filesPath = path.join(__dirname, 'db', '200k_words_100x100');
-// const filesPath = path.join(__dirname, 'db', '2kk_words_400x400');
+// const filesPath = path.join(__dirname, 'db', '200k_words_100x100');
+const filesPath = path.join(__dirname, 'db', '2kk_words_400x400');
 
 // ==================================================================================================
 const getAll = async () => {
@@ -16,8 +16,8 @@ const getAll = async () => {
             let wordsPath = path.join(
               __dirname,
               'db',
-              '200k_words_100x100',
-              // '2kk_words_400x400',
+              // '200k_words_100x100',
+              '2kk_words_400x400',
               filename,
             );
             const dataW = await fs
@@ -30,30 +30,27 @@ const getAll = async () => {
       })
       .catch(error => error);
     const allWords = await getOllWords(data);
-    const ads = [
-      [1, 1, 2, 3, 4],
-      [1, 2, 2, 3, 5],
-      [1, 2, 3, 3, 6],
-      [1, 2, 3, 4, 7],
-    ];
 
-    // const sortArr = allWords.flat().slice().sort();
-    // ----- находим количество всех словосочетаний ---------------------
+    // ----- находим количество всех словосочетаний ----------------------
     const uniqueValues = [...new Set(allWords.flat())];
     console.log('uniqueValues =', uniqueValues.length); // = 129240
 
-    const existInAllFiles = getUniqueElems(allWords);
-    console.log('existInAllFiles =', existInAllFiles.length); // =
+    // const arr = allWords.flat().sort();
+    // const existInAllFiles = getUniqueElems(arr);
+    // console.log('existInAllFiles =', existInAllFiles.length); // =441
+    // const existInAtLeastTen = getExistInAtLeastTen(arr);
+    // console.log('existInAtLeastTen =', existInAtLeastTen.length); // =73245
 
-    // const qwe = existInAtLeastTen(allWords);
-    // console.log('qwe =', qwe.length);
+    const { resAll, resTen } = getUniqueElems(allWords);
+    console.log('existInAllFiles =', resAll.length);
+    console.log('existInAtLeastTen =', resTen.length);
   } catch (error) {
     throw error;
   }
 };
 
 getAll();
-// ----------------------------------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------------
 // функция возвращает масив без дублей [[словосочетания файла-1], [словосочетания файла-2], ...]
 async function getOllWords(data) {
   return data.reduce((acc, el) => {
@@ -61,20 +58,67 @@ async function getOllWords(data) {
     return acc;
   }, []);
 }
+
 // функция возвращает масив элементов встречающихся 20 раз
-function getUniqueElems(ollArr) {
-  const arr = ollArr.flat().sort();
-  const res = [];
+// function getUniqueElems(arr) {
+//   const res = [];
+
+//   arr.reduce((acc, word, i, arr) => {
+//     if (acc.slice(-1)[0] != word) {
+//       if (acc.length === 19) {
+//         res.push(acc[0]);
+//       }
+//       acc.length = 0;
+//     }
+//     if (arr[i + 1] === word) {
+//       acc.push(word);
+//     }
+
+//     return acc;
+//   }, []);
+//   return res;
+// }
+
+// функция возвращает масив элементов встречающихся 10 раз и более
+// function getExistInAtLeastTen(arr) {
+//   const res = [];
+
+//   arr.reduce((acc, word, i, arr) => {
+//     if (acc.slice(-1)[0] != word) {
+//       if (acc.length >= 9) {
+//         res.push(acc[0]);
+//       }
+//       acc.length = 0;
+//     }
+//     if (arr[i + 1] === word) {
+//       acc.push(word);
+//     }
+//     return acc;
+//   }, []);
+//   return res;
+// }
+
+// функция возвращает масив элементов встречающихся (20 раз) && (10 раз и более)
+function getUniqueElems(allWords) {
+  const arr = allWords.flat().sort();
+  const resAll = [];
+  const resTen = [];
 
   arr.reduce((acc, word, i, arr) => {
+    if (acc.slice(-1)[0] != word) {
+      if (acc.length === 19) {
+        resAll.push(acc[0]);
+      }
+      if (acc.length >= 9) {
+        resTen.push(acc[0]);
+      }
+      acc.length = 0;
+    }
     if (arr[i + 1] === word) {
       acc.push(word);
     }
-    if (acc.length === 19) {
-      res.push(acc);
-      acc.length = 0;
-    }
+
     return acc;
   }, []);
-  return res;
+  return { resAll, resTen };
 }
